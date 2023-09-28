9d9b0d363f5a3bbc1f0569e097a241e607cfafa2
Muhammad Zahid <mzmi@kth.se>
2023-09-28 13:14:35 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,109 @@
+# CloudGripper Library: README
+
+The CloudGripper API Client library provides a Python client to communicate with and control robotic arms over a cloud-based API. 
+
+## Features
+
+- Supports multiple robot instances.
+- Simple functions to send commands and fetch robot state.
+- Allows image fetching from robot cameras.
+
+## Prerequisites
+
+Before you start using the CloudGripper client library, ensure the following libraries are installed:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Getting Started
+
+1. **Setting up the Client**:
+
+   First, ensure that your token for the CloudGripper API is set as an environment variable named `CLOUDGRIPPER_TOKEN`.
+
+2. **Importing and Initializing the GripperRobot class**:
+
+   Import the required libraries and classes.
+
+   ```python
+   from client.cloudgripper_client import GripperRobot
+   import os
+   ```
+
+   Initialize the robot object by specifying its name and your API token.
+
+   ```python
+   token = os.environ['CLOUDGRIPPER_TOKEN']
+   robot = GripperRobot('robot3', token)
+   ```
+
+3. **Sending Commands**:
+
+   Use the robot object to send commands and control the robot.
+
+   ```python
+   robot.gripper_open()   # Opens the gripper
+   robot.step_forward()   # Moves the robot forward
+   ```
+
+4. **Fetching Robot State and Images**:
+
+   Retrieve the robot's state:
+
+   ```python
+   state, timestamp = robot.get_state()
+   print(f'state = {state}, time_stamp = {timestamp}')
+   ```
+
+   Fetch images from robot cameras:
+
+   ```python
+   frame, timestamp = robot.getImage()   # Get bottom camera image
+   ```
+
+5. **Streaming Camera Feed**:
+
+   You can also fetch continuous camera feed from the robot and display it using OpenCV.
+
+   ```python
+   import cv2
+
+   while True:
+       image, timestamp = robot.getImageTop()
+       cv2.imshow("Cloudgripper top camera stream", image)
+       if cv2.waitKey(1) & 0xFF == ord('q'):
+           break
+   ```
+
+   Ensure you have OpenCV (`opencv-python`) installed to use this feature.
+
+## Commands Overview
+
+Here are some of the basic commands you can send using the CloudGripper library:
+
+- Movement:
+  - `robot.step_forward()`
+  - `robot.step_backward()`
+  - `robot.step_left()`
+  - `robot.step_right()`
+  
+- Rotation and Movement:
+  - `robot.rotate(angle)`   # Rotation in degrees (0 to 180)
+  - `robot.move_z(z)`       # Move up or down, z value normalized between 0 and 1
+  - `robot.move_xy(x, y)`   # Move to the specific x and y position, x,y normalized between 0 and 1 to cover work area
+
+- Gripper Control:
+  - `robot.gripper_open()`
+  - `robot.gripper_close()`
+
+- Fetch State and Image:
+  - `robot.get_state()`
+  - `robot.getImage()`
+  - `robot.getImageTop()`
+
+## Important Note
+
+- Ensure you have a stable internet connection as the client communicates with the robots over the internet.
+
+---
---
+++ client/cloudgripper_client.py
@@ -0,0 +1,142 @@
+from requests import put, get, post, exceptions
+import cv2
+import base64
+import numpy as np
+
+api_address_robots = {f"robot{i}": f"robot{i}.cloudgripper.org/api/v1.1/robot" for i in range(1, 33)}
+
+
+class GripperRobot:
+    global api_address_robots
+
+    def __init__(self, name, token):
+        self.name = name
+        self.headers = {"Authorization": "Bearer " + token}
+        self.base_api = api_address_robots[name]
+
+    def get_state(self):
+        try:
+            call_api = get(self.base_api + '/state',
+                           headers=self.headers).json()
+            return call_api['state'], call_api['time']
+        except exceptions.RequestException as e:
+            print('Request failed:', e)
+
+    def step_forward(self):
+        try:
+            call_api = get(self.base_api + '/moveUp',
+                           headers=self.headers).json()
+            return call_api['time']
+        except exceptions.RequestException as e:
+            print('Request failed:', e)
+
+    def step_backward(self):
+        try:
+            call_api = get(self.base_api + '/moveDown',
+                           headers=self.headers).json()
+            return call_api['time']
+        except exceptions.RequestException as e:
+            print('Request failed:', e)
+
+    def step_left(self):
+        try:
+            call_api = get(self.base_api + '/moveLeft',
+                           headers=self.headers).json()
+            return call_api['time']
+        except exceptions.RequestException as e:
+            print('Request failed:', e)
+
+    def step_right(self):
+        try:
+            call_api = get(self.base_api + '/moveRight',
+                           headers=self.headers).json()
+            return call_api['time']
+        except exceptions.RequestException as e:
+            print('Request failed:', e)
+
+    def move_gripper(self, angle):
+        self.gripperAngle = angle
+        try:
+            call_api = get(self.base_api + '/grip/' + str(self.gripperAngle),
+                           headers=self.headers).json()
+            return call_api['time']
+        except exceptions.RequestException as e:
+            print('Request failed:', e)
+
+    def gripper_close(self):
+        time_stamp = self.move_gripper(90)
+        return time_stamp
+
+    def gripper_open(self):
+        time_stamp = self.move_gripper(40)
+        return time_stamp
+
+    def rotate(self, angle):
+        self.rotationAngle = angle
+
+        sendAngle = -angle + 180
+        try:
+            call_api = get(self.base_api + '/rotate/' +
+                           str(sendAngle), headers=self.headers).json()
+            return call_api['time']
+        except exceptions.RequestException as e:
+            print('Request failed:', e)
+
+    def move_z(self, z):
+        self.zaxisAngle = z
+        calcVal = (-180/1)*z + 180
+        calcVal = int(calcVal)
+        try:
+            call_api = get(self.base_api + '/up_down/' +
+                           str(calcVal), headers=self.headers).json()
+            return call_api['time']
+        except exceptions.RequestException as e:
+            print('Request failed:', e)
+
+    def move_xy(self, x, y):
+        self.robotPositionX = x
+        self.robotPositionY = y
+
+        x_send = 125*x + 15
+        y_send = 130*y + 30
+        try:
+            call_api = get(self.base_api + '/gcode/' + str(x_send) +
+                           '/' + str(y_send), headers=self.headers).json()
+            return call_api['time']
+        except exceptions.RequestException as e:
+            print('Request failed:', e)
+
+    def calibrate(self):
+        try:
+            get(self.base_api +
+                '/calibrate', headers=self.headers).json()
+        except exceptions.RequestException as e:
+            print('Request failed:', e)
+
+    def getImage(self):
+        try:
+            call_api = get(
+                self.base_api+'/getImage', headers=self.headers).json()
+            getimage = call_api['data']
+            time_stamp = call_api['time']
+            encode_img = getimage.encode('latin1')
+            img = base64.b64decode(encode_img)
+            npimg = np.fromstring(img, dtype=np.uint8)
+            source = cv2.imdecode(npimg, 1)
+            return source, time_stamp
+        except:
+            print("Image not available")
+
+    def getImageTop(self):
+        try:
+            call_api = get(
+                self.base_api+'/getImageTop', headers=self.headers).json()
+            getimage = call_api['data']
+            time_stamp = call_api['time']
+            encode_img = getimage.encode('latin1')
+            img = base64.b64decode(encode_img)
+            npimg = np.fromstring(img, dtype=np.uint8)
+            source = cv2.imdecode(npimg, 1)
+            return source, time_stamp
+        except:
+            print("Image not available")
