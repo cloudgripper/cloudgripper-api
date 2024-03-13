7e6a09a136a82145daef1912a257c67e4a048760
Muhammad Zahid <m.zahid.ilyas77@gmail.com>
2024-03-13 10:20:38 +0100

    update client to work with updated server

--- client/cloudgripper_client.py
+++ client/cloudgripper_client.py
@@ -3,22 +3,21 @@ import cv2
 import base64
 import numpy as np
 
-api_address_robots = {f"robot{i}": f"robot{i}.cloudgripper.org/api/v1.1/robot" for i in range(1, 33)}
-
+api_address_robots = {f"robot{i}": f"https://cloudgripper.zahidmhd.com/robot{i}/api/v1.1/robot" for i in range(1, 33)}
 
 class GripperRobot:
     global api_address_robots
 
     def __init__(self, name, token):
         self.name = name
-        self.headers = {"Authorization": "Bearer " + token}
+        self.headers = {"apiKey": token}
         self.base_api = api_address_robots[name]
 
     def get_state(self):
         try:
-            call_api = get(self.base_api + '/state',
+            call_api = get(self.base_api + '/getState',
                            headers=self.headers).json()
-            return call_api['state'], call_api['time']
+            return call_api['state'], call_api['timestamp']
         except exceptions.RequestException as e:
             print('Request failed:', e)
 
@@ -64,31 +63,27 @@ class GripperRobot:
             print('Request failed:', e)
 
     def gripper_close(self):
-        time_stamp = self.move_gripper(90)
+        time_stamp = self.move_gripper(0)
         return time_stamp
 
     def gripper_open(self):
-        time_stamp = self.move_gripper(40)
+        time_stamp = self.move_gripper(1)
         return time_stamp
 
     def rotate(self, angle):
         self.rotationAngle = angle
-
-        sendAngle = -angle + 180
         try:
             call_api = get(self.base_api + '/rotate/' +
-                           str(sendAngle), headers=self.headers).json()
+                           str(angle), headers=self.headers).json()
             return call_api['time']
         except exceptions.RequestException as e:
             print('Request failed:', e)
 
     def move_z(self, z):
         self.zaxisAngle = z
-        calcVal = (-180/1)*z + 180
-        calcVal = int(calcVal)
         try:
             call_api = get(self.base_api + '/up_down/' +
-                           str(calcVal), headers=self.headers).json()
+                           str(z), headers=self.headers).json()
             return call_api['time']
         except exceptions.RequestException as e:
             print('Request failed:', e)
@@ -96,12 +91,10 @@ class GripperRobot:
     def move_xy(self, x, y):
         self.robotPositionX = x
         self.robotPositionY = y
-
-        x_send = 125*x + 15
-        y_send = 130*y + 30
         try:
-            call_api = get(self.base_api + '/gcode/' + str(x_send) +
-                           '/' + str(y_send), headers=self.headers).json()
+            call_api = get(self.base_api + '/gcode/' + str(x) +
+                           '/' + str(y), headers=self.headers).json()
+            print(call_api)
             return call_api['time']
         except exceptions.RequestException as e:
             print('Request failed:', e)
@@ -113,10 +106,10 @@ class GripperRobot:
         except exceptions.RequestException as e:
             print('Request failed:', e)
 
-    def getImage(self):
+    def getImageBase(self):
         try:
             call_api = get(
-                self.base_api+'/getImage', headers=self.headers).json()
+                self.base_api+'/getImageBase', headers=self.headers).json()
             getimage = call_api['data']
             time_stamp = call_api['time']
             encode_img = getimage.encode('latin1')
