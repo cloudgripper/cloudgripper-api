0b8a1131b827140d46cc4f5f5d669aadc6b515a8
Axel Kaliff <axel.kaliff@protonmail.com>
2024-06-29 16:11:26 +0200

    improved robustness

--- stack_from_scratch/recording.py
+++ stack_from_scratch/recording.py
@@ -1,26 +1,25 @@
-import argparse
 import os
 import sys
 import time
 import json
-from typing import Any, List
-
+from typing import Any, Tuple, List
 import cv2
-from filelock import FileLock
-import numpy as np
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if project_root not in sys.path:
     sys.path.append(project_root)
 
-from library.rgb_object_tracker import all_objects_are_visible
 from client.cloudgripper_client import GripperRobot
-from library.Camera2Robot import *
-from library.utils import get_undistorted_bottom_image, convert_ndarray_to_list
+from library.utils import convert_ndarray_to_list, get_undistorted_bottom_image
 
 
 class Recorder:
-    def __init__(self, session_id, output_dir, m, d, token, idx):
+    FPS = 3
+    FOURCC = cv2.VideoWriter_fourcc(*"mp4v")
+
+    def __init__(
+        self, session_id: str, output_dir: str, m: Any, d: Any, token: str, idx: str
+    ):
         self.stop_flag = False
         self.session_id = session_id
         self.output_dir = output_dir
@@ -28,166 +27,158 @@ class Recorder:
         self.d = d
         self.token = token
         self.robot_idx = idx
-        self.video_writer_top = None
-        self.video_writer_bottom = None
+
         self.frame_counter = 0
         self.video_counter = 0
+        self.video_writer_top = None
+        self.video_writer_bottom = None
+        self.frame_buffer_top: List[Any] = []
+        self.frame_buffer_bottom: List[Any] = []
+
         self.robot = GripperRobot(self.robot_idx, self.token)
-        self.latest_bottom = None
-        self.fourcc = cv2.VideoWriter_fourcc(*"mp4v")
         self.image_top, _ = self.robot.get_image_top()
         self.bottom_image = get_undistorted_bottom_image(self.robot, self.m, self.d)
-        self.fps = 3
 
-        self.frame_buffer_top = []
-        self.frame_buffer_bottom = []
+        self._initialize_directories()
 
-    def _start_new_video(
-        self,
-        output_video_dir,
-        output_bottom_video_dir,
-        video_counter,
-        fourcc,
-        image_shape,
-        bottom_image_shape,
-    ):
+    def _initialize_directories(self):
+        """Initialize output directories."""
+        self.output_video_dir = os.path.join(self.output_dir, "Video")
+        self.output_bottom_video_dir = os.path.join(self.output_dir, "Bottom_Video")
+        self.final_image_dir = os.path.join(self.output_dir, "Final_Image")
+        os.makedirs(self.output_video_dir, exist_ok=True)
+        os.makedirs(self.output_bottom_video_dir, exist_ok=True)
+        os.makedirs(self.final_image_dir, exist_ok=True)
+
+    def _start_new_video(self) -> Tuple[cv2.VideoWriter, cv2.VideoWriter]:
+        """Start new video writers for top and bottom cameras."""
         video_filename_top = os.path.join(
-            output_video_dir, f"video_{video_counter}.mp4"
+            self.output_video_dir, f"video_{self.video_counter}.mp4"
         )
-        video_writer_top = cv2.VideoWriter(video_filename_top, fourcc, self.fps, image_shape)
+        video_writer_top = cv2.VideoWriter(
+            video_filename_top, self.FOURCC, self.FPS, self.image_top.shape[1::-1]
+        )
 
         video_filename_bottom = os.path.join(
-            output_bottom_video_dir, f"video_{video_counter}.mp4"
+            self.output_bottom_video_dir, f"video_{self.video_counter}.mp4"
         )
         video_writer_bottom = cv2.VideoWriter(
-            video_filename_bottom, fourcc, self.fps, bottom_image_shape
+            video_filename_bottom, self.FOURCC, self.FPS, self.bottom_image.shape[1::-1]
         )
 
         return video_writer_top, video_writer_bottom
 
-    def record(self, start_new_video_every=None):
-        self._initialize_directories()
-
-        self.frame_counter = 0
-        self.video_counter = 0
-        self.video_writer_top = None
-        self.video_writer_bottom = None
+    def record(self, start_new_video_every: int = None):
+        """Record video with optional periodic video restarts."""
+        self._prepare_new_recording()
 
         try:
             while not self.stop_flag:
-                imageTop, _ = self.robot.get_image_top()
-                bottom_image = get_undistorted_bottom_image(self.robot, self.m, self.d)
-                self.bottom_image = bottom_image
-                self.image_top = imageTop
-
+                self._capture_frame()
                 if (
-                    start_new_video_every is not None
+                    start_new_video_every
                     and self.frame_counter % start_new_video_every == 0
                 ):
-                    self._start_or_restart_video_writers(imageTop, bottom_image)
+                    self._start_or_restart_video_writers()
                 elif start_new_video_every is None and self.frame_counter == 0:
-                    self._start_or_restart_video_writers(imageTop, bottom_image)
+                    self._start_or_restart_video_writers()
 
-                time.sleep(1 / self.fps)  # maintain the desired FPS
-
-                # Check if the frames are valid
-                if imageTop is not None and bottom_image is not None:
-                    self.frame_buffer_top.append(imageTop)
-                    self.frame_buffer_bottom.append(bottom_image)
+                time.sleep(1 / self.FPS)
+                self._buffer_frames()
 
                 self.save_state(self.robot)
-
                 self.frame_counter += 1
-                print("frames", self.frame_counter)
+                print("Frames recorded:", self.frame_counter)
 
-                cv2.imshow("ImageBottom_" + self.robot_idx, bottom_image)
-
+                cv2.imshow(f"ImageBottom_{self.robot_idx}", self.bottom_image)
                 if cv2.waitKey(1) & 0xFF == ord("q"):
                     self.stop_flag = True
         except Exception as e:
             print(f"An error occurred: {e}")
         finally:
             self._write_frames_to_video()
-            self.release_writers()
+            self._release_writers()
             cv2.destroyAllWindows()
 
-    def _start_or_restart_video_writers(self, imageTop, bottom_image):
+    def _capture_frame(self):
+        """Capture frames from the robot's cameras."""
+        try:
+            self.image_top, _ = self.robot.get_image_top()
+            self.bottom_image = get_undistorted_bottom_image(self.robot, self.m, self.d)
+        except Exception as e:
+            print(f"Error capturing frame: {e}")
+
+    def _buffer_frames(self):
+        """Buffer the captured frames."""
+        if self.image_top is not None and self.bottom_image is not None:
+            self.frame_buffer_top.append(self.image_top)
+            self.frame_buffer_bottom.append(self.bottom_image)
+        else:
+            print("Frame capture failed, skipping buffer update")
+
+    def _start_or_restart_video_writers(self):
+        """Start or restart video writers."""
         self._write_frames_to_video()
-
-        self.video_writer_top, self.video_writer_bottom = self._start_new_video(
-            self.output_video_dir,
-            self.output_bottom_video_dir,
-            self.video_counter,
-            self.fourcc,
-            (imageTop.shape[1], imageTop.shape[0]),
-            (bottom_image.shape[1], bottom_image.shape[0]),
-        )
-
+        self._release_writers()  # Ensure the old writers are released
+        self.video_writer_top, self.video_writer_bottom = self._start_new_video()
         self.video_counter += 1
 
-
     def _write_frames_to_video(self):
-        if self.video_writer_top is not None and self.video_writer_bottom is not None:
-            if len(self.frame_buffer_top) > 0 and len(self.frame_buffer_bottom) > 0:
-                for frame_top, frame_bottom in zip(self.frame_buffer_top, self.frame_buffer_bottom):
-                    try:
-                        self.video_writer_top.write(frame_top)
-                        self.video_writer_bottom.write(frame_bottom)
-                    except Exception as e:
-                        print(f"Error writing frame to video: {e}")
-                self.frame_buffer_top = []
-                self.frame_buffer_bottom = []
+        """Write buffered frames to the video files."""
+        if self.video_writer_top and self.video_writer_bottom:
+            for frame_top, frame_bottom in zip(
+                self.frame_buffer_top, self.frame_buffer_bottom
+            ):
+                try:
+                    self.video_writer_top.write(frame_top)
+                    self.video_writer_bottom.write(frame_bottom)
+                except cv2.error as e:
+                    print(f"Error writing frame to video: {e}")
+            self.frame_buffer_top.clear()  # Clear buffer after writing
+            self.frame_buffer_bottom.clear()  # Clear buffer after writing
 
-
-    def release_writers(self):
-        if self.video_writer_top is not None:
+    def _release_writers(self):
+        """Release the video writers."""
+        if self.video_writer_top:
             self.video_writer_top.release()
-        if self.video_writer_bottom is not None:
+        if self.video_writer_bottom:
             self.video_writer_bottom.release()
 
     def write_final_image(self):
-        print("writing final image")
-
-        imageTop, _ = self.robot.get_image_top()
-        cv2.imwrite(
-            os.path.join(self.final_image_dir, f"final_image_{self.video_counter}.jpg"),
-            imageTop,
+        """Write the final image from the top camera."""
+        print("Writing final image")
+        image_top, _ = self.robot.get_image_top()
+        final_image_path = os.path.join(
+            self.final_image_dir, f"final_image_{self.video_counter}.jpg"
         )
+        cv2.imwrite(final_image_path, image_top)
 
-    def _initialize_directories(self):
-        self.output_video_dir = os.path.join(self.output_dir, "Video")
-        self.output_bottom_video_dir = os.path.join(self.output_dir, "Bottom_Video")
-        self.final_image_dir = os.path.join(self.output_dir, "Final_Image")
-
-        os.makedirs(self.output_video_dir, exist_ok=True)
-        os.makedirs(self.output_bottom_video_dir, exist_ok=True)
-        os.makedirs(self.final_image_dir, exist_ok=True)
-
-    def start_new_recording(self, new_output_dir):
+    def start_new_recording(self, new_output_dir: str):
+        """Start a new recording session with a new output directory."""
         self.output_dir = new_output_dir
+        self._initialize_directories()
+        self._prepare_new_recording()
+        print(f"Started new recording in directory: {new_output_dir}")
+
+    def _prepare_new_recording(self):
+        """Prepare for a new recording session."""
         self.frame_counter = 0
         self.video_counter = 0
         self.stop_flag = False
-        self._initialize_directories()
-        self._start_or_restart_video_writers(self.image_top, self.bottom_image)
-        print(f"Started new recording in directory: {new_output_dir}")
+        self._start_or_restart_video_writers()
 
     def stop(self):
+        """Set the stop flag to terminate recording."""
         self.stop_flag = True
         print("Stop flag set to True")
 
-    
-
-    def save_state(
-        self,
-        robot: Any,
-    ):
+    def save_state(self, robot: Any):
+        """Save the state of the robot to a JSON file."""
         state, timestamp = robot.get_state()
         state = convert_ndarray_to_list(state)
         state["time"] = timestamp
 
         state_file = os.path.join(self.output_dir, "states.json")
-
         if os.path.exists(state_file):
             with open(state_file, "r") as file:
                 data = json.load(file)
@@ -198,36 +189,3 @@ class Recorder:
 
         with open(state_file, "w") as file:
             json.dump(data, file, indent=4)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Robot Index")
-    parser.add_argument("--robot_idx", type=str, required=True, help="Robot Index")
-    parser.add_argument(
-        "--output_dir", help="The prefix for the directories", required=True
-    )
-
-    args = parser.parse_args()
-    idx = args.robot_idx
-    prefix = args.output_dir
-
-    token = os.getenv("ROBOT_TOKEN", "default_token")
-
-    m, d = (
-        [
-            [505.24537524391866, 0.0, 324.5096286632362],
-            [0.0, 505.6456651337437, 233.54118730278543],
-            [0.0, 0.0, 1.0],
-        ],
-        [
-            [-0.07727407195057368],
-            [-0.047989733519315944],
-            [0.12157420705123315],
-            [-0.09667542135039282],
-        ],
-    )
-
-    output_dir = prefix
-    recorder = Recorder("test", output_dir, m, d, token, idx)
-    recorder.record(start_new_video_every=30)
-
