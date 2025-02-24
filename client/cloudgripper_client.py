64c057d9d4eb2987a098f6fd31dab0051516809d
Muhammad Zahid <mzmi@kth.se>
2025-02-24 12:59:59 +0100

    Add docstrings for cloudgripper client

--- client/cloudgripper_client.py
+++ client/cloudgripper_client.py
@@ -6,6 +6,13 @@ import numpy as np
 api_address_robots = {f"robot{i}": f"https://cloudgripper.eecs.kth.se:8443/robot{i}/api/v1.1/robot" for i in range(1, 33)}
 
 class GripperRobot:
+    """
+    A class to represent a CloudGripper robot
+
+    Args:
+        name (str): The name of the robot
+        token (str): The token of the robot
+    """
     global api_address_robots
 
     def __init__(self, name, token):
@@ -14,6 +21,24 @@ class GripperRobot:
         self.base_api = api_address_robots[name]
 
     def get_state(self):
+        """
+        Get the current state of the robot
+
+        Args:
+            None
+        
+        Returns:
+            state (dict): current state of the robot:
+                - 'x_norm': Normalized x-coordinate of the robot's position
+                - 'y_norm': Normalized y-coordinate of the robot's position
+                - 'z_norm': Normalized z-coordinate of the robot's position
+                - 'rotation': Current rotation angle of the robot (in degrees)
+                - 'claw_norm': Normalized state of the robot's gripper (0 for closed, 1 for open)
+                - 'z_current': Current in the z-axis servo motor
+                - 'rotation_current': Current in the rotation servo motor
+                - 'claw_current': Current in the claw servo motor
+            timestamp (float): timestamp of the state in seconds since the epoch
+        """
         try:
             call_api = get(self.base_api + '/getState',
                            headers=self.headers).json()
@@ -22,6 +47,15 @@ class GripperRobot:
             print('Request failed:', e)
 
     def step_forward(self):
+        """
+        Move the robot one step forward in the y-direction
+
+        Args:
+            None
+
+        Returns:
+            timestamp (float): timestamp of the command in seconds since the epoch
+        """
         try:
             call_api = get(self.base_api + '/moveUp',
                            headers=self.headers).json()
@@ -30,6 +64,15 @@ class GripperRobot:
             print('Request failed:', e)
 
     def step_backward(self):
+        """
+        Move the robot one step backward in the y-direction
+
+        Args:
+            None
+
+        Returns:
+            timestamp (float): timestamp of the command in seconds since the epoch
+        """
         try:
             call_api = get(self.base_api + '/moveDown',
                            headers=self.headers).json()
@@ -38,6 +81,15 @@ class GripperRobot:
             print('Request failed:', e)
 
     def step_left(self):
+        """
+        Move the robot one step left in the x-direction
+
+        Args:
+            None
+
+        Returns:
+            timestamp (float): timestamp of the command in seconds since the epoch
+        """
         try:
             call_api = get(self.base_api + '/moveLeft',
                            headers=self.headers).json()
@@ -46,6 +98,15 @@ class GripperRobot:
             print('Request failed:', e)
 
     def step_right(self):
+        """
+        Move the robot one step right in the x-direction
+
+        Args:
+            None
+
+        Returns:
+            timestamp (float): timestamp of the command in seconds since the epoch
+        """
         try:
             call_api = get(self.base_api + '/moveRight',
                            headers=self.headers).json()
@@ -54,6 +115,15 @@ class GripperRobot:
             print('Request failed:', e)
 
     def move_gripper(self, angle):
+        """
+        Move the robot's gripper to the specified angle
+
+        Args:
+            angle (float): The desired angle for the gripper (0 for closed, 1 for open)
+
+        Returns:
+            timestamp (float): Timestamp of the command in seconds since the epoch
+        """
         self.gripperAngle = angle
         try:
             call_api = get(self.base_api + '/grip/' + str(self.gripperAngle),
@@ -63,14 +133,41 @@ class GripperRobot:
             print('Request failed:', e)
 
     def gripper_close(self):
+        """
+        Close the robot's gripper
+
+        Args:
+            None
+
+        Returns:
+            timestamp (float): Timestamp of the command in seconds since the epoch
+        """
         time_stamp = self.move_gripper(0)
         return time_stamp
 
     def gripper_open(self):
+        """
+        Open the robot's gripper
+
+        Args:
+            None
+
+        Returns:
+            timestamp (float): Timestamp of the command in seconds since the epoch
+        """
         time_stamp = self.move_gripper(1)
         return time_stamp
 
     def rotate(self, angle):
+        """
+        Rotate the robot to the specified angle
+
+        Args:
+            angle (float): The desired rotation angle for the robot (in degrees)
+
+        Returns:
+            timestamp (float): Timestamp of the command in seconds since the epoch
+        """
         self.rotationAngle = angle
         try:
             call_api = get(self.base_api + '/rotate/' +
@@ -80,6 +177,15 @@ class GripperRobot:
             print('Request failed:', e)
 
     def move_z(self, z):
+        """
+        Move the robot's z-axis to the specified normalized position
+
+        Args:
+            z (float): The desired z-axis position for the robot (0 for fully down, 1 for fully up)
+
+        Returns:
+            timestamp (float): Timestamp of the command in seconds since the epoch
+        """
         self.zaxisAngle = z
         try:
             call_api = get(self.base_api + '/up_down/' +
@@ -89,6 +195,16 @@ class GripperRobot:
             print('Request failed:', e)
 
     def move_xy(self, x, y):
+        """
+        Move the robot to the specified normalized x and y coordinates
+
+        Args:
+            x (float): The desired normalized x-coordinate for the robot (0 for leftmost, 1 for rightmost)
+            y (float): The desired normalized y-coordinate for the robot (0 for backmost, 1 for forwardmost)
+
+        Returns:
+            timestamp (float): Timestamp of the command in seconds since the epoch
+        """
         self.robotPositionX = x
         self.robotPositionY = y
         try:
@@ -100,6 +216,15 @@ class GripperRobot:
             print('Request failed:', e)
 
     def calibrate(self):
+        """
+        (INACTIVE) Calibrate the robot's position and orientation
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         try:
             get(self.base_api +
                 '/calibrate', headers=self.headers).json()
@@ -107,6 +232,16 @@ class GripperRobot:
             print('Request failed:', e)
 
     def getImageBase(self):
+        """
+        Get the base camera image from the robot
+
+        Args:
+            None
+
+        Returns:
+            image (numpy.ndarray): The base camera image as a numpy array
+            time_stamp (float): Timestamp of the image in seconds since the epoch
+        """
         try:
             call_api = get(
                 self.base_api+'/getImageBase', headers=self.headers).json()
@@ -121,6 +256,16 @@ class GripperRobot:
             print("Image not available")
 
     def getImageTop(self):
+        """
+        Get the top camera image from the robot
+
+        Args:
+            None
+
+        Returns:
+            image (numpy.ndarray): The top camera image as a numpy array
+            time_stamp (float): Timestamp of the image in seconds since the epoch
+        """
         try:
             call_api = get(
                 self.base_api+'/getImageTop', headers=self.headers).json()
